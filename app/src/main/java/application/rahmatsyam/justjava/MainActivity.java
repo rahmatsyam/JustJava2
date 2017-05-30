83205b5359b989a3a0232c631b1950a7e26e6d66
Rahmat Syam <akyrisa03@gmail.com>
2017-05-30 10:17:07 +0800

    komentar jumlah

--- app/src/main/java/application/rahmatsyam/justjava/MainActivity.java
+++ app/src/main/java/application/rahmatsyam/justjava/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
+    //jumlah cofee
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
